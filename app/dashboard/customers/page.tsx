73366a10f2fe953aefc6f12273e6cc47dfe94f20
Aguri Cano <aguricano@gmail.com>
2026-05-19 01:10:55 -0500

    chapter 11

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Suspense } from "react";
+import { SearchParams } from "next/dist/shared/lib/router/utils/parse-path";
+
+interface PageProps {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export default async function Page({ searchParams }: PageProps) {
+  const query =
+    typeof searchParams?.query === "string" ? searchParams.query : "";
+  const customers = await fetchFilteredCustomers(query);
+  return <CustomersTable customers={customers} />;
 }
